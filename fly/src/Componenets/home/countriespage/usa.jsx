9b684490f1f7929e6046153748f2556a65578d1b
jaineel sampat <142526912+JaineelSampat@users.noreply.github.com>
2024-03-28 01:21:29 +0530

    Update and rename usa.html to usa.jsx

---
+++ fly/src/Componenets/home/countriespage/usa.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+
+const USAInfo = () => {
+  return (
+    <div>
+      <h2>UNITED STATES OF AMERICA</h2>
+      <div className="container">
+        <div className="image">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRa6_GTK-PjdsU_ebbPocomguprFm7oXiY7rw&usqp=CAU"
+            alt="usa"
+            width="200"
+          />
+        </div>
+        <p>
+          The United States is a vast country located in North America, bordered
+          by Canada to the north and Mexico to the south. It spans a diverse
+          range of geographical features, including vast plains, towering
+          mountain ranges such as the Rocky Mountains and the Appalachian
+          Mountains, expansive forests like the Redwood forests in California,
+          and diverse coastlines along the Atlantic and Pacific Oceans. The
+          country also encompasses diverse climates, from arid deserts in the
+          southwest to temperate forests in the northeast and tropical climates
+          in Hawaii and parts of Florida
+        </p>
+        <div className="image right">
+          <img
+            src="https://as2.ftcdn.net/v2/jpg/02/70/88/67/1000_F_270886793_RiROA4PV64kbsBQ5eeXfY0hgEMzseuEw.jpg"
+            alt="usa culture"
+            width="200"
+          />
+        </div>
+        <p>
+          The culture of the United States is incredibly diverse, reflecting its
+          history of immigration and multiculturalism. It is often described as
+          a "melting pot" where people from various ethnic, religious, and
+          cultural backgrounds contribute to a rich tapestry of traditions,
+          languages, and customs. American culture has had a profound global
+          influence, particularly in areas such as music, film, literature, and
+          technology. The concept of the "American Dream," representing the
+          pursuit of opportunity, freedom, and success, is deeply ingrained in
+          the national ethos
+        </p>
+        <div className="image">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVGd2zE6aDXwHZZD3QC5RiMfZ1eGsChU-WVQ&usqp=CAU"
+            alt="victorian architecture"
+            width="200"
+            height="150"
+          />
+        </div>
+        <p>
+          American architecture is characterized by a blend of different styles
+          influenced by various historical periods and cultural movements. From
+          iconic landmarks like the Statue of Liberty and the White House to
+          modern skyscrapers like the Empire State Building and the Willis
+          Tower, the architectural landscape of the United States is diverse
+          and dynamic. Notable architectural styles include Colonial, Victorian,
+          Art Deco, Modernist, and Postmodernist architecture, each reflecting
+          different periods of American history and cultural influences.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default USAInfo;
